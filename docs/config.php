97619eff102fb3fd01d3e7f2861f03920aa6c191
王志鹏 <123389141@qq.com>
2015-02-03 10:18:03 +0800

    独立docs项目

--- docs/config.php
+++
@@ -1,8 +0,0 @@
-<?php
- #SSH配置
- define("TOOLDOCS","D:\\elong\\HotelTestTeam\\files\\documents\\toolDocs");
- define("SSH_PORT","22");
- define("SSH_USER","tester");
- define("SSH_PASS","nopass.2");
-
-?>
